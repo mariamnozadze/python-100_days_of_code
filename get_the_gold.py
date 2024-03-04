03e98f52babdd36b1d977eae06c624ee44c0affa
Mariam Nozadze <75265035+mariamnozadze@users.noreply.github.com>
2024-03-04 21:59:51 +0400

    Update get_the_gold.py

--- get_the_gold.py
+++ get_the_gold.py
@@ -4,7 +4,7 @@ choice1 = input("You're at a turning point, left or right? ").lower()
 if choice1 == "right":
     choice2 = input("There's an open door in front of you, room is dark. You can't see anything there. Wait or go ? ").lower()
     if choice2 == "wait":
-        choice3 = input("Which door would you step into ? Red, Blue or Yellow ? ")
+        choice3 = input("Which door would you step into ? Red, Blue or Yellow ? ").lower()
         if choice3 == "red":
             print("You won ! ")
         else:
